b54852e65dfe120d010337badf3a9537c810349c
gosha <44@.ru>
2025-12-15 22:50:34 +0500

    add project

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector('.burger');
+const burgerClose = document.querySelector('.burger--close');
+const headerWrapperMobile = document.querySelector('.header__wrapper--mobile');
+const body = document.body;
+
+burger.addEventListener("click", () => {
+    headerWrapperMobile.classList.add('active');
+    body.classList.add('stop-scroll');
+})
+
+burgerClose.addEventListener("click", () => {
+    headerWrapperMobile.classList.remove('active');
+    body.classList.remove('stop-scroll');
+})
