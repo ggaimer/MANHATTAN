195aa7d679fe17f152cc0615c3a04b095ed285a1
gosha <44@.ru>
2025-12-17 21:25:35 +0500

    add section menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
-const burger = document.querySelector('.burger');
-const burgerClose = document.querySelector('.burger--close');
-const headerWrapperMobile = document.querySelector('.header__wrapper--mobile');
-const body = document.body;
+const burger = document.querySelector('.burger')
+const burgerClose = document.querySelector('.burger--close')
+const headerWrapperMobile = document.querySelector('.header__wrapper--mobile')
+const body = document.body
 
 burger.addEventListener("click", () => {
-    headerWrapperMobile.classList.add('active');
-    body.classList.add('stop-scroll');
+    headerWrapperMobile.classList.add('active')
+    body.classList.add('stop-scroll')
 })
 
 burgerClose.addEventListener("click", () => {
-    headerWrapperMobile.classList.remove('active');
-    body.classList.remove('stop-scroll');
+    headerWrapperMobile.classList.remove('active')
+    body.classList.remove('stop-scroll')
+})
+
+
+// swiper
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1.1,
+            spaceBetween: 16,
+        },
+
+        768: {
+            slidesPerView: 1,
+        },
+    }
 })
