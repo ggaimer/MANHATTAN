834f495ced609ee62deb2536ab41d27d6948052c
gosha <44@.ru>
2025-12-21 23:04:07 +0500

    добавил страницу - Афишы и адаптировал

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,68 @@ const swiper = new Swiper('.swiper', {
 
 // accordion
 
+// let slideUp = (target, duration = 300) => {
+// 	target.style.transitionProperty = 'height, margin, padding'
+// 	target.style.transitionDuration = duration + 'ms'
+// 	target.style.boxSizing = 'border-box'
+// 	target.style.height = target.offsetHeight + 'px'
+// 	target.offsetHeight
+// 	target.style.overflow = 'hidden'
+// 	target.style.height = 0
+// 	target.style.paddingTop = 0
+// 	target.style.paddingBottom = 0
+// 	target.style.marginTop = 0
+// 	target.style.marginBottom = 0
+// 	target.style.border = 'none'
+
+// 	window.setTimeout(() => {
+// 		target.style.display = 'none'
+// 		target.style.removeProperty('height')
+// 		target.style.removeProperty('padding-top')
+// 		target.style.removeProperty('padding-bottom')
+// 		target.style.removeProperty('margin-top')
+// 		target.style.removeProperty('margin-bottom')
+// 		target.style.removeProperty('overflow')
+// 		target.style.removeProperty('transition-duration')
+// 		target.style.removeProperty('transition-property')
+// 		target.style.removeProperty('border')
+// 	}, duration)
+// }
+// /* SLIDE DOWN */
+// let slideDown = (target, duration = 300) => {
+// 	target.style.removeProperty('display')
+// 	let display = window.getComputedStyle(target).display
+// 	if (display === 'none') display = 'grid'
+// 	target.style.display = display
+// 	let height = target.offsetHeight
+// 	target.style.overflow = 'hidden'
+// 	target.style.height = 0
+// 	target.style.paddingTop = 0
+// 	target.style.paddingBottom = 0
+// 	target.style.marginTop = 0
+// 	target.style.marginBottom = 0
+// 	target.offsetHeight
+// 	target.style.boxSizing = 'border-box'
+// 	target.style.transitionProperty = 'height, margin, padding'
+// 	target.style.transitionDuration = duration + 'ms'
+// 	target.style.height = height + 'px'
+// 	target.style.border = 'none'
+
+// 	target.style.removeProperty('padding-top')
+// 	target.style.removeProperty('padding-bottom')
+// 	target.style.removeProperty('margin-top')
+// 	target.style.removeProperty('margin-bottom')
+// 	target.style.removeProperty('border')
+
+// 	window.setTimeout(() => {
+// 		target.style.removeProperty('height')
+// 		target.style.removeProperty('overflow')
+// 		target.style.removeProperty('transition-duration')
+// 		target.style.removeProperty('transition-property')
+// 		target.style.removeProperty('border')
+// 	}, duration)
+// }
+
 const accordions = document.querySelectorAll('.party__accordion');
 
 accordions.forEach(accordion => {
@@ -49,9 +111,9 @@ accordions.forEach(accordion => {
         accordions.forEach(item => {
             if (item !== accordion) {
                 item.classList.remove('party__accordion--open');
+            } else {
+                accordion.classList.toggle('party__accordion--open');
             }
         });
-
-        accordion.classList.toggle('party__accordion--open');
     });
 });
